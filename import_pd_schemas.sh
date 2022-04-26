1583e422af35d6dcccd2f1921347a2d69db39170
CKAN service account <ckan@tbs-regweb01.t1.tbs-sct.gc.ca>
2022-04-26 13:44:11 +0000

    Add bash versions of the import_pd_schemas and csv scripts

---
+++ import_pd_schemas.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Re-read the recombinant schemas from the Open Canada portal and import them into the PD Tracker database.
+# No arguments are required.
+#
+
+# activate the PD Tracker Python virtual environment before running.
+source ./venv/bin/activate
+
+declare -a pdtypes=("adminaircraft" "ati" "briefingt" "consultations" "contracts" "contractsa" \
+                    "dac", "experiment" "grants" "hospitalityq" "inventory" "nap" "qpnotes" \
+                    "reclassification" "service" "travela" "travelq" "wrongdoing")
+
+# Call the Django custom command to re-read the schemas from the Open Canada portal.
+for pdtype in "${pdtypes[@]}"
+do
+  python ./manage.py import_ckan_schema  -t $pdtype
+  echo "Imported $pdtype"
+done
+date
+
